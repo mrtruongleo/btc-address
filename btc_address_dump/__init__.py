e2dfe78da40a0e47c7c2254787bbb18e15619e9b
mrtruongleo <66201727+mrtruongleo@users.noreply.github.com>
2023-05-30 09:46:43 +0900

    Update __init__.py

--- btc_address_dump/__init__.py
+++ btc_address_dump/__init__.py
@@ -1,10 +1,5 @@
 """btc-address-dump.
 A utility for dump btc address from mnemonic words or private key or public key.
 """
-from .btc_address_dump import main_entry
+from .btc_address_dump import get_addr
 
-import sys
-
-
-def run_main():
-    main_entry(sys.argv)
